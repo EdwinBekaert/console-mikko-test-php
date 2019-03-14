9d4b773056b50ff14b9db6a4270e9fd03c270595
Edwin Bekaert <48248357+EdwinBekaert@users.noreply.github.com>
2019-03-14 21:47:51 +0100

    cleanup

--- tests/Service/PaydayTest.php
+++ tests/Service/PaydayTest.php
@@ -23,7 +23,7 @@ class PaydayTest extends TestCase
         $payday = new Payday($today);
         $thisMonth = (int)date('n', $today);
 
-        $result = $payday->getPaydayForMonth($thisMonth,date('j', $today),MissedStrategy::LastFriday);
+        $result = $payday->getPaydayForMonth($thisMonth,date('j', $today), MissedStrategy::LastFriday);
 
         // assert that result is null
         $this->assertEquals(null, $result);
@@ -31,20 +31,20 @@ class PaydayTest extends TestCase
 
     public function testPaydayCompliesWithStrategy()
     {
-        $today = strtotime('2019-03-14'); // march has last day in weekenddate('d-m-Y', $paydayBonus)date('d-m-Y', $paydayBonus)
+        $today = strtotime('2019-03-14'); // march has last day in weekend
         $payday = new Payday($today);
         $dayOfPay = '31';
         $thisMonth = 3; // a month of 31 days
 
-        $result = $payday->getPaydayForMonth($thisMonth, $dayOfPay,MissedStrategy::LastFriday);
+        $result = $payday->getPaydayForMonth($thisMonth, $dayOfPay, MissedStrategy::LastFriday);
         // assert that payday is friday 29/03/2019
         $this->assertEquals(strtotime('2019-03-29'), $result);
 
-        $result = $payday->getPaydayForMonth($thisMonth, $dayOfPay,MissedStrategy::NextMonday);
+        $result = $payday->getPaydayForMonth($thisMonth, $dayOfPay, MissedStrategy::NextMonday);
         // assert that payday is monday 01/04/2019
         $this->assertEquals(strtotime('2019-04-01'), $result);
 
-        $result = $payday->getPaydayForMonth($thisMonth, $dayOfPay,MissedStrategy::NextWednesday);
+        $result = $payday->getPaydayForMonth($thisMonth, $dayOfPay, MissedStrategy::NextWednesday);
         // assert that payday is monday 01/04/2019
         $this->assertEquals(strtotime('2019-04-03'), $result);
 
@@ -52,13 +52,13 @@ class PaydayTest extends TestCase
 
     public function testPaydayIn30dayMonthOrLessWorks()
     {
-        $today = strtotime('2019-01-14'); // march has last day in weekenddate('d-m-Y', $paydayBonus)date('d-m-Y', $paydayBonus)
+        $today = strtotime('2019-01-14'); // march has last day in weekend
         $payday = new Payday($today);
         // assert that payday in april is 30/04/2019
-        $result = $payday->getPaydayForMonth(4, 't',MissedStrategy::LastFriday);
+        $result = $payday->getPaydayForMonth(4, 't', MissedStrategy::LastFriday);
         $this->assertEquals(strtotime('2019-04-30'), $result);
         // assert that payday in february is 28/02/2019
-        $result = $payday->getPaydayForMonth(2, 't',MissedStrategy::LastFriday);
+        $result = $payday->getPaydayForMonth(2, 't', MissedStrategy::LastFriday);
         $this->assertEquals(strtotime('2019-02-28'), $result);
 
     }
@@ -69,7 +69,7 @@ class PaydayTest extends TestCase
         $payday = new Payday($today);
         $thisMonth = (int)date('n', $today);
 
-        $result = $payday->getPaydayForMonth($thisMonth,'31',MissedStrategy::NextMonday);
+        $result = $payday->getPaydayForMonth($thisMonth,'31', MissedStrategy::NextMonday);
 
         // assert that result is null
         $this->assertEquals(null, $result);
